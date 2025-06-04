edd09eba4842c5beeeaa1734f741d40d5f6fa18e
juyeonu <juyeonu221@gmail.com>
2025-06-04 17:19:20 +0900

    1차 구현 완료
    오른쪽 컨트롤러 A를 누르면 대화가 시작

--- Assets/ConversationStarter.cs
+++ Assets/ConversationStarter.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 using DialogueEditor;
+using UnityEngine.XR;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class ConversationStarter : MonoBehaviour
 {
-    [SerializeField] private NPCConversation myConversation;
+    [SerializeField] public NPCConversation myConversation;
+
+    private InputDevice rightController;
+
+    void Start()
+    {
+        var rightHandDevices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHandDevices);
+        if (rightHandDevices.Count > 0) { rightController = rightHandDevices[0]; }
+    }
+
+
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("player"))
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (!rightController.isValid) return;
+            
+            bool aButtonPressed = false;
+
+            if (rightController.TryGetFeatureValue(CommonUsages.primaryButton,out aButtonPressed)&&aButtonPressed) 
             {
                 ConversationManager.Instance.StartConversation(myConversation);
             }
