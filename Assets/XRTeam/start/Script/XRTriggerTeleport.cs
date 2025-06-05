98012203b524348a0a5006551ffb478fc8ff544e
KimEunJu <dmswn7253@gmail.com>
2025-06-05 16:29:26 +0900

    토끼 대화 수정 중

--- Assets/XRTeam/start/Script/XRTriggerTeleport.cs
+++ Assets/XRTeam/start/Script/XRTriggerTeleport.cs
@@ -6,7 +6,7 @@ public class XRTriggerTeleport : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("TeleportTartget"))  // Plane 오브젝트의 태그
+        if (other.CompareTag("Teleport"))  // Plane 오브젝트의 태그
         {
             // XR Origin의 위치를 Spawn으로 이동
             transform.position = spawnPoint.position;
